9e5f867fa6d1d51c5462741c83ef8159db704f4a
Daniel <o.dani@yahoo.com>
2016-06-06 21:38:38 +0300

    error fix

--- app/src/main/java/ro/ratt/transport/MapHandler.java
+++ app/src/main/java/ro/ratt/transport/MapHandler.java
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.util.ArrayList;
